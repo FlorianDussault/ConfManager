23666df51024b0ae9fb7e73e1909495482e33d98
Florian <floriandussault@hotmail.com>
2022-06-22 10:25:56 +0200

    Delete publish.cmd

--- ConfManager/publish.cmd
+++
@@ -1,5 +0,0 @@
-c:\temp\nuget.exe pack -Properties Configuration=Release
-
-pause
-
-c:\temp\nuget.exe push ConfManager.6.0.0.nupkg oy2akcq2d4tzzss2r36qnhl44vlmeobefglxwm7d6lndo4 -Source https://api.nuget.org/v3/index.json
